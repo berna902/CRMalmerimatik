760e364f713dc5af999de16bd11cab14087b2ecb
berna902 <berna85als@gmail.com>
2014-07-08 17:40:55 +0200

    antes de empezar servicio

--- ServicioCRM/ISrvDatos.cs
+++ ServicioCRM/ISrvDatos.cs
@@ -7,27 +7,11 @@ using System.Web;
 
 namespace almerimatik.ServicioCRM
 {
-    public class ISrvDatos
+    [ServiceContract]
+    public interface ISrvDatos
     {
-        [ServiceContract]
-        public interface ISrvDatos
-        {
-            [OperationContract]
-            List<LibroData> GetAllLibros();
-            [OperationContract]
-            bool GuardarLibro(LibroData libro);
-        }
-        [DataContract]
-        public class LibroData
-        {
-            [DataMember]
-            public string isbn;
-            [DataMember]
-            public string titulo;
-            [DataMember]
-            public string Tema;
-            [DataMember]
-            public string portada;
-        }
+        
     }
+    
+    
 }
