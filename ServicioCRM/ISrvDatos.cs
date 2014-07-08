96a6708d3dfb7309bc680d950e1cf89264f72312
berna902 <berna85als@gmail.com>
2014-07-08 17:45:39 +0200

    mas clases

--- ServicioCRM/ISrvDatos.cs
+++ ServicioCRM/ISrvDatos.cs
@@ -10,8 +10,21 @@ namespace almerimatik.ServicioCRM
     [ServiceContract]
     public interface ISrvDatos
     {
-        
+        [OperationContract]
+        List<LibroData> GetAllLibros();
+        [OperationContract]
+        bool GuardarLibro(LibroData libro);
     }
-    
-    
+    [DataContract]
+    public class LibroData
+    {
+        [DataMember]
+        public string isbn;
+        [DataMember]
+        public string titulo;
+        [DataMember]
+        public string Tema;
+        [DataMember]
+        public string portada;
+    }
 }
---
+++ ServicioCRM/Models/TelefonosEmpresa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServicioCRM.Models
+{
+    public class TelefonosEmpresa: TelefonoEmpresa
+    {
+
+    }
+}
