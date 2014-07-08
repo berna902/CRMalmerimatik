5dac0bcc81d02aee324769dbf3f97a3ed13081f3
rguezmoralaura <rguezmoralaura@gmail.com>
2014-07-08 16:59:37 +0200

    Creado SVRDatos

---
+++ ServicioCRM/ISrvDatos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Web;
+
+namespace almerimatik.ServicioCRM
+{
+    public class ISrvDatos
+    {
+        [ServiceContract]
+        public interface ISrvDatos
+        {
+            [OperationContract]
+            List<LibroData> GetAllLibros();
+            [OperationContract]
+            bool GuardarLibro(LibroData libro);
+        }
+        [DataContract]
+        public class LibroData
+        {
+            [DataMember]
+            public string isbn;
+            [DataMember]
+            public string titulo;
+            [DataMember]
+            public string Tema;
+            [DataMember]
+            public string portada;
+        }
+    }
+}
