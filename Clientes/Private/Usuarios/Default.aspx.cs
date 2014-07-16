ef30152a7ea2aecc9f75a599af14b2a66393b37d
rguezmoralaura <rguezmoralaura@gmail.com>
2014-07-16 16:39:16 +0200

    dsfsfs

---
+++ Clientes/Private/Usuarios/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using Clientes.ServicioDatos2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clientes.Private.Usuarios
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                SrvDatosClient proxy = new SrvDatosClient();
+
+                UserData[] usuarios = proxy.GetAllUsers();
+
+                this.GridView1.DataSource = usuarios;
+                this.GridView1.DataBind();
+
+            }
+        }
+    }
+}
